a1dfe908429591b46bbb7c247b8d5122bbf5ae98
Tord Jansson <tord.jansson@gmail.com>
2011-09-10 14:54:41 +0200

    Added color-defines for web-colors.

--- source/wg_color.cpp
+++ source/wg_color.cpp
@@ -13,7 +13,7 @@
   version 2 of the License, or (at your option) any later version.
 
                             -----------
-	
+
   The WonderGUI Graphics Toolkit is also available for use in commercial
   closed-source projects under a separate license. Interested parties
   should contact Tord Jansson [tord.jansson@gmail.com] for details.
@@ -22,9 +22,157 @@
 
 #include <wg_color.h>
 
-WgColor	WgColor::g_white(0xFFFFFFFF);
-WgColor	WgColor::g_black(0xFF000000);
-WgColor	WgColor::g_none(0x00FFFFFF);
+const WgColor WgColor::transparent		( 0x00FFFFFF );
+
+const WgColor WgColor::aliceblue		( 0xFFF0F8FF );
+const WgColor WgColor::antiquewhite		( 0xFFFAEBD7 );
+const WgColor WgColor::aqua				( 0xFF00ffff );
+const WgColor WgColor::aquamarine		( 0xFF7fffd4 );
+const WgColor WgColor::azure			( 0xFFf0ffff );
+const WgColor WgColor::beige			( 0xFFf5f5dc );
+const WgColor WgColor::bisque  			( 0xFFffe4c4 );
+const WgColor WgColor::black	  		( 0xFF000000 );
+const WgColor WgColor::blanchedalmond 	( 0xFFffebcD );
+const WgColor WgColor::blue 	  		( 0xFF0000ff );
+const WgColor WgColor::blueviolet 		( 0xFF8a2be2 );
+const WgColor WgColor::brown   			( 0xFFa52a2a );
+const WgColor WgColor::burlywood  		( 0xFFdeb887 );
+const WgColor WgColor::cadetblue  		( 0xFF5f9ea0 );
+const WgColor WgColor::chartreuse 		( 0xFF7fff00 );
+const WgColor WgColor::chocolate  		( 0xFFd2691e );
+const WgColor WgColor::coral   			( 0xFFff7f50 );
+const WgColor WgColor::cornflowerblue 	( 0xFF6495eD );
+const WgColor WgColor::cornsilk 		( 0xFFfff8dc );
+const WgColor WgColor::crimson  		( 0xFFdc143c );
+const WgColor WgColor::cyan  			( 0xFF00ffff );
+const WgColor WgColor::darkblue  		( 0xFF00008b );
+const WgColor WgColor::darkcyan  		( 0xFF008b8b );
+const WgColor WgColor::darkgoldenrod  	( 0xFFb8860b );
+const WgColor WgColor::darkgray  		( 0xFFa9a9a9 );
+const WgColor WgColor::darkgrey  		( 0xFFa9a9a9 );
+const WgColor WgColor::darkgreen  		( 0xFF006400 );
+const WgColor WgColor::darkkhaki  		( 0xFFbdb76b );
+const WgColor WgColor::darkmagenta  	( 0xFF8b008b );
+const WgColor WgColor::darkolivegreen 	( 0xFF556b2F );
+const WgColor WgColor::darkorange  		( 0xFFff8c00 );
+const WgColor WgColor::darkorchid  		( 0xFF9932cc );
+const WgColor WgColor::darkred  		( 0xFF8b0000 );
+const WgColor WgColor::darksalmon  		( 0xFFe9967a );
+const WgColor WgColor::darkseagreen  	( 0xFF8fbc8f );
+const WgColor WgColor::darkslateblue  	( 0xFF483d8b );
+const WgColor WgColor::darkslategray  	( 0xFF2f4f4f );
+const WgColor WgColor::darkslategrey  	( 0xFF2f4f4f );
+const WgColor WgColor::darkturquoise  	( 0xFF00ced1 );
+const WgColor WgColor::darkviolet  		( 0xFF9400d3 );
+const WgColor WgColor::deeppink  		( 0xFFff1493 );
+const WgColor WgColor::deepskyblue 		( 0xFF00bfff );
+const WgColor WgColor::dimgray  		( 0xFF696969 );
+const WgColor WgColor::dimgrey  		( 0xFF696969 );
+const WgColor WgColor::dodgerblue  		( 0xFF1e90ff );
+const WgColor WgColor::firebrick  		( 0xFFb22222 );
+const WgColor WgColor::floralwhite 		( 0xFFfffaf0 );
+const WgColor WgColor::forestgreen 		( 0xFF228b22 );
+const WgColor WgColor::fuchsia  		( 0xFFff00ff );
+const WgColor WgColor::gainsboro  		( 0xFFdcdcdc );
+const WgColor WgColor::ghostwhite  		( 0xFFf8f8ff );
+const WgColor WgColor::gold  			( 0xFFffd700 );
+const WgColor WgColor::goldenrod  		( 0xFFdaa520 );
+const WgColor WgColor::gray  			( 0xFF808080 );
+const WgColor WgColor::grey  			( 0xFF808080 );
+const WgColor WgColor::green  			( 0xFF008000 );
+const WgColor WgColor::greenyellow 		( 0xFFadff2f );
+const WgColor WgColor::honeydew  		( 0xFFf0fff0 );
+const WgColor WgColor::hotpink  		( 0xFFff69b4 );
+const WgColor WgColor::indianred   		( 0xFFcd5c5c );
+const WgColor WgColor::indigo   		( 0xFF4b0082 );
+const WgColor WgColor::ivory  			( 0xFFfffff0 );
+const WgColor WgColor::khaki  			( 0xFFf0e68c );
+const WgColor WgColor::lavender  		( 0xFFe6e6fa );
+const WgColor WgColor::lavenderblush  	( 0xFFfff0f5 );
+const WgColor WgColor::lawngreen  		( 0xFF7cfc00 );
+const WgColor WgColor::lemonchiffon		( 0xFFfffacd );
+const WgColor WgColor::lightblue  		( 0xFFadd8e6 );
+const WgColor WgColor::lightcoral  		( 0xFFf08080 );
+const WgColor WgColor::lightcyan  		( 0xFFe0ffff );
+const WgColor WgColor::lightgoldenrodyellow	( 0xFFfAFAD2 );
+const WgColor WgColor::lightgray  		( 0xFFd3d3d3 );
+const WgColor WgColor::lightgrey  		( 0xFFd3d3d3 );
+const WgColor WgColor::lightgreen  		( 0xFF90ee90 );
+const WgColor WgColor::lightpink  		( 0xFFffb6c1 );
+const WgColor WgColor::lightsalmon  	( 0xFFffa07a );
+const WgColor WgColor::lightseagreen  	( 0xFF20b2aa );
+const WgColor WgColor::lightskyblue  	( 0xFF87cefa );
+const WgColor WgColor::lightslategray 	( 0xFF778899 );
+const WgColor WgColor::lightslategrey 	( 0xFF778899 );
+const WgColor WgColor::lightsteelblue 	( 0xFFb0c4dE );
+const WgColor WgColor::lightyellow  	( 0xFFffffe0 );
+const WgColor WgColor::lime  			( 0xFF00ff00 );
+const WgColor WgColor::limegreen  		( 0xFF32cd32 );
+const WgColor WgColor::linen  			( 0xFFfaf0e6 );
+const WgColor WgColor::magenta  		( 0xFFff00ff );
+const WgColor WgColor::maroon  			( 0xFF800000 );
+const WgColor WgColor::mediumaquamarine ( 0xFF66cdaA );
+const WgColor WgColor::mediumblue  		( 0xFF0000cd );
+const WgColor WgColor::mediumorchid		( 0xFFba55d3 );
+const WgColor WgColor::mediumpurple		( 0xFF9370d8 );
+const WgColor WgColor::mediumseagreen 	( 0xFF3cb371 );
+const WgColor WgColor::mediumslateblue  ( 0xFF7b68eE );
+const WgColor WgColor::mediumspringgreen( 0xFF00fa9A );
+const WgColor WgColor::mediumturquoise  ( 0xFF48d1cC );
+const WgColor WgColor::mediumvioletred  ( 0xFFc71585 );
+const WgColor WgColor::midnightblue  	( 0xFF191970 );
+const WgColor WgColor::mintcream  		( 0xFFf5fffa );
+const WgColor WgColor::mistyrose  		( 0xFFffe4e1 );
+const WgColor WgColor::moccasin  		( 0xFFffe4b5 );
+const WgColor WgColor::navajowhite  	( 0xFFffdead );
+const WgColor WgColor::navy  			( 0xFF000080 );
+const WgColor WgColor::oldlace  		( 0xFFfdf5e6 );
+const WgColor WgColor::olive  			( 0xFF808000 );
+const WgColor WgColor::olivedrab  		( 0xFF6b8e23 );
+const WgColor WgColor::orange  			( 0xFFffa500 );
+const WgColor WgColor::orangered  		( 0xFFff4500 );
+const WgColor WgColor::orchid  			( 0xFFda70d6 );
+const WgColor WgColor::palegoldenrod  	( 0xFFeee8aa );
+const WgColor WgColor::palegreen  		( 0xFF98fb98 );
+const WgColor WgColor::paleturquoise  	( 0xFFafeeee );
+const WgColor WgColor::palevioletred  	( 0xFFd87093 );
+const WgColor WgColor::papayawhip  		( 0xFFffefd5 );
+const WgColor WgColor::peachpuff  		( 0xFFffdab9 );
+const WgColor WgColor::peru  			( 0xFFcd853f );
+const WgColor WgColor::pink  			( 0xFFffc0cb );
+const WgColor WgColor::plum  			( 0xFFdda0dd );
+const WgColor WgColor::powderblue  		( 0xFFb0e0e6 );
+const WgColor WgColor::purple  			( 0xFF800080 );
+const WgColor WgColor::red  			( 0xFFff0000 );
+const WgColor WgColor::rosybrown  		( 0xFFbc8f8f );
+const WgColor WgColor::royalblue  		( 0xFF4169e1 );
+const WgColor WgColor::saddlebrown 		( 0xFF8b4513 );
+const WgColor WgColor::salmon  			( 0xFFfa8072 );
+const WgColor WgColor::sandybrown  		( 0xFFf4a460 );
+const WgColor WgColor::seagreen  		( 0xFF2e8b57 );
+const WgColor WgColor::seashell  		( 0xFFfff5ee );
+const WgColor WgColor::sienna  			( 0xFFa0522d );
+const WgColor WgColor::silver  			( 0xFFc0c0c0 );
+const WgColor WgColor::skyblue  		( 0xFF87ceeb );
+const WgColor WgColor::slateblue  		( 0xFF6a5acd );
+const WgColor WgColor::slategray  		( 0xFF708090 );
+const WgColor WgColor::slategrey  		( 0xFF708090 );
+const WgColor WgColor::snow  			( 0xFFfffafa );
+const WgColor WgColor::springgreen 		( 0xFF00ff7f );
+const WgColor WgColor::steelblue  		( 0xFF4682b4 );
+const WgColor WgColor::tan  			( 0xFFd2b48c );
+const WgColor WgColor::teal  			( 0xFF008080 );
+const WgColor WgColor::thistle  		( 0xFFd8bfd8 );
+const WgColor WgColor::tomato  			( 0xFFff6347 );
+const WgColor WgColor::turquoise  		( 0xFF40e0d0 );
+const WgColor WgColor::violet  			( 0xFFee82ee );
+const WgColor WgColor::wheat  			( 0xFFf5deb3 );
+const WgColor WgColor::white  			( 0xFFffffff );
+const WgColor WgColor::whitesmoke  		( 0xFFf5f5f5 );
+const WgColor WgColor::yellow  			( 0xFFffff00 );
+const WgColor WgColor::yellowgreen 		( 0xFF9acd32 );
+
+
 
 
 //____ blend() ________________________________________________________________
