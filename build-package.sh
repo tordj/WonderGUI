0a2da1d7c70393b09738cf4b20b699025af1e2a6
Tord Jansson <tord@wondergui.com>
2014-12-18 10:07:45 +0200

    Version 2.0-alpha1
    Changes from previous release:
    
    * Cleaned up and redesigned directory structure.
    * Added makefile for Linux/Unix environments.
    * Added solution for VisualStudio 2013.
    * Added README-files to various places.

--- build-package.sh
+++ build-package.sh
@@ -6,14 +6,19 @@ ROOT="$PWD"
 cd $ROOT
 cd build/gnumake
 make clean
+rm ~*
 
 cd $ROOT
 cd build/codelite-linux
-rm -rf Debug Release *.txt *.mk Makefile compile_commands.json .build-debug .build-release .clang .codelite
+rm -rf Debug Release *.txt *.mk *~ Makefile compile_commands.json .build-debug .build-release .clang .codelite
+
+cd $ROOT
+cd build
+rm *~
 
 cd $ROOT
 cd doc
-rm -rf Doxyfile~ doxygen_sqlite3.db html latex
+rm -rf *~ doxygen_sqlite3.db html latex
 
 cd $ROOT
 rm -f $PACKAGE
