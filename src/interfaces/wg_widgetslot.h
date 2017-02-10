a19f60ec36dbdb6e847068eeaf75616ce10b7633
Tord Jansson <tord.jansson@gmail.com>
2017-02-10 18:19:51 +0100

    Added interface WidgetSlot.

---
+++ src/interfaces/wg_widgetslot.h
@@ -0,0 +1,79 @@
+/*=========================================================================
+
+                         >>> WonderGUI <<<
+
+  This file is part of Tord Jansson's WonderGUI Graphics Toolkit
+  and copyright (c) Tord Jansson, Sweden [tord.jansson@gmail.com].
+
+                            -----------
+
+  The WonderGUI Graphics Toolkit is free software; you can redistribute
+  this file and/or modify it under the terms of the GNU General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+                            -----------
+
+  The WonderGUI Graphics Toolkit is also available for use in commercial
+  closed-source projects under a separate license. Interested parties
+  should contact Tord Jansson [tord.jansson@gmail.com] for details.
+
+=========================================================================*/
+
+#ifndef	WG_WIDGETSLOT_DOT_H
+#define	WG_WIDGETSLOT_DOT_H
+
+#ifndef WG_INTERFACE_DOT_H
+#	include <wg_interface.h>
+#endif
+
+#ifndef WG_POINTERS_DOT_H
+#	include <wg_pointers.h>
+#endif
+
+#ifndef WG_WIDGET_DOT_H
+#	include <wg_widget.h>
+#endif
+
+namespace wg 
+{
+	class WidgetSlot;
+	typedef	StrongInterfacePtr<WidgetSlot,Interface_p>	WidgetSlot_p;
+	typedef	WeakInterfacePtr<WidgetSlot,Interface_wp>	WidgetSlot_wp;
+	
+	class WidgetSlot : public Interface
+	{
+		
+	public:
+		virtual bool			isInstanceOf( const char * pClassName ) const;
+		virtual const char *	className( void ) const;
+		static const char		CLASSNAME[];
+		static WidgetSlot_p		cast( const Interface_p& pInterface );
+	
+		inline WidgetSlot operator=(const Widget_p& pWidget );
+		inline operator Widget_p() const;
+
+		inline bool operator==(const Widget_p& other) const;
+		inline bool operator!=(const Widget_p& other) const;
+	
+		inline operator bool() const;
+
+		inline Widget& operator*() const;
+		inline Widget* operator->() const;
+
+
+		void clear();
+	
+	
+
+	protected:
+		virtual Object * 	_object() const;
+		
+		
+		
+	};
+	
+	
+
+} // namespace wg
+#endif //WG_WidgetSlot_DOT_H
