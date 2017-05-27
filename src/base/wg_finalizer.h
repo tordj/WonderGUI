142032997d928d886ca38ff4bba3ea944596b664
Tord Jansson <tord.jansson@gmail.com>
2017-05-27 08:43:31 +0200

    Update CodeLite project and added documentation.

--- src/base/wg_finalizer.h
+++ src/base/wg_finalizer.h
@@ -34,6 +34,30 @@ namespace wg
 	class Finalizer;
 	typedef	StrongPtr<Finalizer>		Finalizer_p;
 	typedef	WeakPtr<Finalizer>	Finalizer_wp;
+
+	/**
+	 * @brief Run custom code when an object is destroyed
+	 * 
+	 * The Finalizer is an object which can attach itself to one or more objects and call
+	 * a function when any of them are destroyed.
+	 * 
+	 * The Finalizer function is called right before the objects destructor is called, so the
+	 * object is still fully intact and functional.
+	 * 
+	 * A small, extra object is created for each object that the Finalizer is attached to,
+	 * resulting in an extra heap allocation and minor memory overhead.
+	 *
+	 * An object can only be attached to one Finalizer. An attempt to attach a Finalizer to an
+	 * object that already is attached to another Finalizer will fail. The object needs 
+	 * to be detached from the other Finalizer first.
+	 * 
+	 * A Finalizer is an Object itself, so you can actually attach a Finalizer to another Finalizer.
+	 * This can be useful if you want a function to be called when the last of several objects is
+	 * destroyed. Simply attach all objects to the same Finalizer which you then attach to a second Finalizer.
+	 * As long as you don't have any StrongPointers keeping the first Finalizer alive, it will be destroyed
+	 * when the last attached Object is destroyed, causing the second Finalizer to call its function.
+	 * 
+	*/
 	
 	class Finalizer : public Object
 	{
