7c927b84f4c2b47b03fa634dca1df4316280ac43
Tord Jansson <tord@nordicuitech.com>
2022-01-08 18:46:19 +0100

    Fixed Doxyfile to support our macros and use new file structure.

--- doc/make_docs.bat
+++ doc/make_docs.bat
@@ -2,7 +2,7 @@
 mkdir temp
 rm -f temp/*
 rm -rf html
-cp ../src/base/* ../src/fonts/**/* ../src/components/* ../src/gfxdevices/**/* ../src/sizebrokers/* ../src/skins/* ../src/skins/**/* ../src/textmappers/* ../src/valueformatters/* ../src/widgets/* ../src/widgets/**/* temp
+cp ../src/baselibrary/base/* ../src/fontengines/**/* ../src/baselibrary/components/* ../src/gfxdevices/**/* ../src/baselibrary/sizebrokers/* ../src/baselibrary/skins/* ../src/baselibrary/skins/**/* ../src/baselibrary/textlayouts/* ../src/baselibrary/numberlayouts/* ../src/baselibrary/widgets/* ../src/baselibrary/widgets/**/* temp
 ruby ../scripts/predoxy.rb temp/*.h
 doxygen
 ruby ../scripts/postdoxy.rb html/class*.html
