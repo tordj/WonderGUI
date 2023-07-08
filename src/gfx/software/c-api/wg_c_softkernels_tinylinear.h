324ece8e264b45ebc7696ffb6703f1daf909151b
Tord Jansson <tord@nordicuitech.com>
2023-07-08 11:51:11 +0200

    SoftGfxDevice: Added TinyLinear SoftKernels.

--- src/gfx/software/c-api/wg_c_softkernels_tinylinear.h
+++ src/gfx/software/c-api/wg_c_softkernels_tinylinear.h
@@ -1,7 +1,7 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-	int	wg_addDefaultSoftKernels( wg_obj device );
+	int	wg_addLinearTinySoftKernels( wg_obj device );
 #ifdef __cplusplus
 }
 #endif
---
+++ src/gfx/software/wg_softkernels_tinylinear.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <wg_softgfxdevice.h>
+
+namespace wg
+{
+	bool addLinearTinySoftKernels( SoftGfxDevice * pDevice );
+};
