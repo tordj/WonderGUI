fd41064df6b4ed2718578c7837a0f821c443486f
Tord Jansson <tord@nordicuitech.com>
2024-01-16 10:01:39 +0100

    Fixed Metal build error + GfxDeviceTester missing file.

---
+++ src/tools/gfxdevicetester/testsuites/blurtests.h
@@ -0,0 +1,96 @@
+#include "testsuite.h"
+
+class BlurTests : public TestSuite
+{
+public:
+	BlurTests()
+	{
+		name = "BlurTests";
+
+		addTest("Blur", &BlurTests::setBlurMatrices, &BlurTests::blur, &BlurTests::dummy);
+	}
+
+	bool init(GfxDevice * pDevice, const RectI& canvas, WonderApp::Visitor * pAppVisitor)
+	{
+//		m_pImg = pAppVisitor->loadSurface("resources/mipmaptest.png", pDevice->surfaceFactory());
+//		if (!m_pImg)
+//			return false;
+
+		m_pCanvasSurface = pDevice->surfaceFactory()->createSurface( { .size = canvas.size()/64, .canvas = true } );
+		m_pBlurSurface[0] = pDevice->surfaceFactory()->createSurface( { .size = canvas.size()/64, .canvas = true, .format = PixelFormat::BGRX_8 } );
+		m_pBlurSurface[1] = pDevice->surfaceFactory()->createSurface( { .size = canvas.size()/64, .canvas = true, .format = PixelFormat::BGRX_8 } );
+
+		return true;
+	}
+	 
+	bool exit(GfxDevice * pDevice, const RectI& canvas)
+	{
+		m_pCanvasSurface = nullptr;
+		m_pBlurSurface[0] = nullptr;
+		m_pBlurSurface[1] = nullptr;
+		return true;
+	}
+
+	bool dummy(GfxDevice * pDevice, const RectI& canvas)
+	{
+		return true;
+	}
+
+	bool setBlurMatrices(GfxDevice * pDevice, const RectI& canvas)
+	{
+		m_pBlurSurface[0]->fill( HiColor::Transparent );
+		m_pBlurSurface[1]->fill( HiColor::Transparent );
+
+		float redMtx[9] = {	0.14f,0.1f,0.14f,
+							0.1f, 0.0f, 0.1f,
+							0.14f, 0.1f, 0.14f};
+		
+		float blueMtx[9] = {0.15f, 0.15f, 0.15f, 0, 0.4f, 0, 0,0,0};
+		float greenMtx[9] = {0,0,0, 0,0.7f,0, 0,0,0};
+		
+		pDevice->setBlurMatrices(64*4, redMtx, greenMtx, blueMtx);
+
+		pDevice->beginCanvasUpdate(m_pCanvasSurface);
+		pDevice->setBlendMode(BlendMode::Replace);
+		pDevice->fill(HiColor::Transparent);
+		pDevice->fill( RectSPX(100,100,64,64)*64, HiColor::White);
+		pDevice->endCanvasUpdate();
+
+		for( int i = 0 ; i < 2 ; i++ )
+		{
+			pDevice->beginCanvasUpdate(m_pBlurSurface[i]);
+			pDevice->setBlendMode(BlendMode::Replace);
+			pDevice->fill(HiColor::Black);
+			pDevice->endCanvasUpdate();
+		}
+		return true;
+ 
+	}
+
+
+	bool blur(GfxDevice * pDevice, const RectI& canvas)
+	{
+		for( int i = 0 ; i < 10 ; i++ )
+		{
+			pDevice->beginCanvasUpdate(m_pBlurSurface[0]);
+			pDevice->fill({100*64,100*64,100*64,100*64}, HiColor::White);
+			pDevice->endCanvasUpdate();
+
+			pDevice->beginCanvasUpdate(m_pBlurSurface[1]);
+			pDevice->setBlitSource(m_pBlurSurface[0]);
+			pDevice->blur({0,0});
+			pDevice->endCanvasUpdate();
+
+			std::swap( m_pBlurSurface[0], m_pBlurSurface[1] );
+		}
+		
+		pDevice->setBlitSource(m_pBlurSurface[0]);
+		pDevice->blur(canvas);
+		return true;
+	}
+
+
+private:
+	Surface_p	m_pCanvasSurface;
+	Surface_p	m_pBlurSurface[2];
+};
