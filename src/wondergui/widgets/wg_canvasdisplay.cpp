a01a3758548d340dafcdb01b34a2f5f96666b9f1
Tord Jansson <tord@nordicuitech.com>
2024-03-01 15:54:54 +0100

    CanvasDisplay: Added support for tintColor/tintGradient/blendMode with transitions.

--- src/wondergui/widgets/wg_canvasdisplay.cpp
+++ src/wondergui/widgets/wg_canvasdisplay.cpp
@@ -94,6 +94,74 @@ namespace wg
 		}
 	}
 
+	//____ setTintColor() ______________________________________________________
+
+	void CanvasDisplay::setTintColor(HiColor color, ColorTransition* pTransition)
+	{
+		if (color != m_tintColor)
+		{
+			if (pTransition)
+			{
+				if (!m_pTintColorTransition)
+					_startReceiveUpdates();
+
+				m_pTintColorTransition = pTransition;
+				m_tintColorTransitionProgress = 0;
+				m_startTintColor = m_tintColor;
+				m_endTintColor = color;
+			}
+			else
+			{
+				if (m_pTintColorTransition)
+					_stopReceiveUpdates();
+
+				m_pTintColorTransition = nullptr;
+				m_tintColor = color;
+				_requestRender();
+			}
+		}
+	}
+
+	//____ setTintGradient() __________________________________________________
+
+	void CanvasDisplay::setTintGradient(const Gradient& gradient, ColorTransition* pTransition)
+	{
+		if (gradient != m_gradient)
+		{
+			if (pTransition)
+			{
+				if (!m_pGradientTransition)
+					_startReceiveUpdates();
+
+				m_pGradientTransition = pTransition;
+				m_gradientTransitionProgress = 0;
+				m_startGradient = m_gradient;
+				m_endGradient = gradient;
+			}
+			else
+			{
+				if (m_pGradientTransition)
+					_stopReceiveUpdates();
+
+				m_pGradientTransition = nullptr;
+				m_gradient = gradient;
+				_requestRender();
+			}
+			
+		}
+	}
+
+	//____ setBlendMode() __________________________________________________________
+
+	void CanvasDisplay::setBlendMode(BlendMode mode)
+	{
+		if (mode != m_blendMode)
+		{
+			m_blendMode = mode;
+			_requestRender();
+		}
+	}
+
 	//____ defaultSize() __________________________________________________________
 
 	SizeSPX CanvasDisplay::_defaultSize(int scale) const
@@ -112,6 +180,79 @@ namespace wg
 			return Widget::_defaultSize(scale);
 	}
 
+	//____ _update() __________________________________________________________
+
+	void CanvasDisplay::_update(int microPassed, int64_t microsecTimestamp)
+	{
+		if (m_pTintColorTransition)
+		{
+			int timestamp = m_tintColorTransitionProgress + microPassed;
+
+			if (timestamp >= m_pTintColorTransition->duration())
+			{
+				m_tintColorTransitionProgress = 0;
+				m_pTintColorTransition = nullptr;
+				m_tintColor = m_endTintColor;
+
+				if (m_tintColor != m_endTintColor)
+				{
+					m_tintColor = m_endTintColor;
+					_requestRender();
+				}
+
+				_stopReceiveUpdates();
+			}
+			else
+			{
+				m_tintColorTransitionProgress = timestamp;
+				HiColor color = m_pTintColorTransition->snapshot(timestamp, m_startTintColor, m_endTintColor);
+
+				if (color != m_tintColor)
+				{
+					m_tintColor = color;
+					_requestRender();
+				}
+			}
+		}
+
+		if (m_pGradientTransition)
+		{
+			int timestamp = m_gradientTransitionProgress + microPassed;
+
+			if (timestamp >= m_pGradientTransition->duration())
+			{
+				m_gradientTransitionProgress = 0;
+				m_pGradientTransition = nullptr;
+
+				if (m_gradient != m_endGradient)
+				{
+					m_gradient = m_endGradient;
+					_requestRender();
+				}
+
+				_stopReceiveUpdates();
+			}
+			else
+			{
+				m_gradientTransitionProgress = timestamp;
+
+				Gradient gradient;
+
+				gradient.topLeft = m_pGradientTransition->snapshot(timestamp, m_startGradient.topLeft, m_endGradient.topLeft);
+				gradient.topRight = m_pGradientTransition->snapshot(timestamp, m_startGradient.topRight, m_endGradient.topRight);
+				gradient.bottomLeft = m_pGradientTransition->snapshot(timestamp, m_startGradient.bottomLeft, m_endGradient.bottomLeft);
+				gradient.bottomRight = m_pGradientTransition->snapshot(timestamp, m_startGradient.bottomRight, m_endGradient.bottomRight);
+
+				if (gradient != m_gradient)
+				{
+					m_gradient = gradient;
+					_requestRender();
+				}
+			}
+		}
+	}
+
+
 	//____ render() ___________________________________________________________
 
 	void CanvasDisplay::_render(GfxDevice* pDevice, const RectSPX& _canvas, const RectSPX& _window)
@@ -126,10 +267,33 @@ namespace wg
 			{
 				auto pSurface = m_pCanvas->_renderCanvas(pDevice);
 
+				auto canvasArea = _canvasWindow(dest);
+				
+				// Blit our canvas
 
+//				int		rl = pDevice->renderLayer();
+				BlendMode bm = pDevice->blendMode();
+				HiColor c = pDevice->tintColor();
+
+//				if (m_renderLayer != -1)
+//					pDevice->setRenderLayer(m_renderLayer);
+
+				pDevice->setBlendMode(m_blendMode);
+
+				pDevice->setTintColor(m_tintColor);
+
+				if (m_gradient.isValid() )
+					pDevice->setTintGradient(canvasArea, m_gradient);
 
 				pDevice->setBlitSource(pSurface);
-				pDevice->stretchBlit(_canvasWindow(dest) );
+				pDevice->stretchBlit(canvasArea);
+
+				if (m_gradient.isValid())
+					pDevice->clearTintGradient();
+
+				pDevice->setTintColor(c);
+				pDevice->setBlendMode(bm);
+//				pDevice->setRenderLayer(rl);
 			}
 		}
 	}
