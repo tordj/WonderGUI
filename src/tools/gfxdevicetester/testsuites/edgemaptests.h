fc9de0030d76c5755abe02921f526c0c637672a4
Tord Jansson <tord@nordicuitech.com>
2024-09-04 16:35:15 +0200

    gfxdevice-ng: Fixed kernel-messup from previous commit. Added some tests.

--- src/tools/gfxdevicetester/testsuites/edgemaptests.h
+++ src/tools/gfxdevicetester/testsuites/edgemaptests.h
@@ -8,11 +8,21 @@ public:
 		name = "EdgemapTests";
 
 		addTest("Edgemap SPX topdown", &EdgemapTests::spxTopDown );
-		addTest("Edgemap SPX topdown flip 90", &EdgemapTests::spxTopDown90 );
+		addTest("Edgemap SPX topdown rot90", &EdgemapTests::spxTopDown90 );
 
 		addTest("Edgemap SPX gradients X", &EdgemapTests::spxTopDownGradientsX );
 		addTest("Edgemap SPX gradients Y", &EdgemapTests::spxTopDownGradientsY );
 		addTest("Edgemap SPX gradients XY", &EdgemapTests::spxTopDownGradientsXY );
+
+		addTest("Edgemap grad X rot90", &EdgemapTests::spxTopDownGradientsX90 );
+		addTest("Edgemap grad Y rot90", &EdgemapTests::spxTopDownGradientsY90 );
+		addTest("Edgemap grad XY rot90", &EdgemapTests::spxTopDownGradientsXY90 );
+
+		addTest("Edgemap grad XY flipX", &EdgemapTests::spxTopDownGradientsXYFlipX );
+		addTest("Edgemap grad XY flipY", &EdgemapTests::spxTopDownGradientsXYFlipY );
+
+		addTest("Edgemap grad XY rot90 flipX", &EdgemapTests::spxTopDownGradientsXYRot90FlipX );
+		addTest("Edgemap grad XY rot90 flipY", &EdgemapTests::spxTopDownGradientsXYRot90FlipY );
 
 	}
 
@@ -52,7 +62,7 @@ public:
 		{
 			tintmapsX[i] = Gradyent::create(Color::White, Color::White, m_rainbowColors[i], Color::Black);
 			tintmapsY[i] = Gradyent::create(m_rainbowColors[i], Color::Black, Color::White, Color::White);
-			tintmapsXY[i] = Gradyent::create(m_rainbowColors[i], Color::Black, Color::White, Color::Black);
+			tintmapsXY[i] = Gradyent::create(m_rainbowColors[i], Color::Black, Color::Red, Color::Green);
 		}
 		
 		
@@ -98,6 +108,54 @@ public:
 		return true;
 	}
 
+	bool spxTopDownGradientsX90(GfxDevice * pDevice, const RectI& canvas)
+	{
+		
+		pDevice->flipDrawEdgemap(canvas, m_pEdgemapSPXGradientX, GfxFlip::Rot90 );
+		return true;
+	}
+
+	bool spxTopDownGradientsY90(GfxDevice * pDevice, const RectI& canvas)
+	{
+		
+		pDevice->flipDrawEdgemap(canvas, m_pEdgemapSPXGradientY, GfxFlip::Rot90 );
+		return true;
+	}
+
+	bool spxTopDownGradientsXY90(GfxDevice * pDevice, const RectI& canvas)
+	{
+		
+		pDevice->flipDrawEdgemap(canvas, m_pEdgemapSPXGradientXY, GfxFlip::Rot90 );
+		return true;
+	}
+
+	bool spxTopDownGradientsXYFlipX(GfxDevice * pDevice, const RectI& canvas)
+	{
+		pDevice->flipDrawEdgemap(canvas, m_pEdgemapSPXGradientXY, GfxFlip::FlipX );
+		return true;
+	}
+
+	bool spxTopDownGradientsXYFlipY(GfxDevice * pDevice, const RectI& canvas)
+	{
+		pDevice->flipDrawEdgemap(canvas, m_pEdgemapSPXGradientXY, GfxFlip::FlipY );
+		return true;
+	}
+
+	bool spxTopDownGradientsXYRot90FlipX(GfxDevice * pDevice, const RectI& canvas)
+	{
+		
+		pDevice->flipDrawEdgemap(canvas, m_pEdgemapSPXGradientXY, GfxFlip::Rot90FlipX );
+		return true;
+	}
+
+	bool spxTopDownGradientsXYRot90FlipY(GfxDevice * pDevice, const RectI& canvas)
+	{
+		
+		pDevice->flipDrawEdgemap(canvas, m_pEdgemapSPXGradientXY, GfxFlip::Rot90FlipY );
+		return true;
+	}
+
+	
 	
 	bool spxTopDown90(GfxDevice * pDevice, const RectI& canvas)
 	{
