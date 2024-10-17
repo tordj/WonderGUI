fa255b9e9791faad6d2c04ab42fb6496b4fa10a5
Tord Jansson <tord@nordicuitech.com>
2024-10-17 17:00:41 +0200

    WIP: Edgemap refactoring to replace Tintmaps with colorstrips. Does not compile.

--- src/extensions/softgfx/wg_softedgemap.h
+++ src/extensions/softgfx/wg_softedgemap.h
@@ -34,63 +34,68 @@ namespace wg
 	typedef WeakPtr<SoftEdgemap> SoftEdgemap_wp;
 
 	class SoftEdgemap : public Edgemap
-	{
-		friend class SoftGfxDevice;
-		friend class SoftBackend;
+{
+	friend class SoftGfxDevice;
+	friend class SoftBackend;
+	
+public:
+	
+	//.____ Creation __________________________________________
+	
+	static SoftEdgemap_p	create( const Blueprint& blueprint );
+	static SoftEdgemap_p	create( const Blueprint& blueprint, SampleOrigo origo, const float * pSamples, int edges, int edgePitch = 0, int samplePitch = 0);
+	static SoftEdgemap_p	create( const Blueprint& blueprint, SampleOrigo origo, const spx * pSamples, int edges, int edgePitch = 0, int samplePitch = 0);
+	
+	
+	//.____ Identification __________________________________________
+	
+	const TypeInfo&     typeInfo(void) const override;
+	const static TypeInfo   TYPEINFO;
+	
+	//.____ Appearance ____________________________________________________
+	
+	bool	setColors( int begin, int end, const HiColor * pColors ) override;
+	bool	setGradients( int begin, int end, const Gradient * pGradients ) override;
+	bool	setTintmaps( int begin, int end, const Tintmap_p * pTintmaps ) override;
+	
+	//.____ Content _______________________________________________________
+	
+	bool 	importSamples( SampleOrigo origo, const spx * pSource, int edgeBegin, int edgeEnd,
+						  int sampleBegin, int sampleEnd, int edgePitch = 0, int samplePitch = 0 ) override;
+	
+	bool 	importSamples( SampleOrigo origo, const float * pSource, int edgeBegin, int edgeEnd,
+						  int sampleBegin, int sampleEnd, int edgePitch = 0, int samplePitch = 0 ) override;
+	
+	bool 	exportSamples( SampleOrigo origo, spx * pDestination, int edgeBegin, int edgeEnd,
+						  int sampleBegin, int sampleEnd, int edgePitch = 0, int samplePitch = 0 ) override;
+	
+	bool 	exportSamples( SampleOrigo origo, float * pDestination, int edgeBegin, int edgeEnd,
+						  int sampleBegin, int sampleEnd, int edgePitch = 0, int samplePitch = 0 ) override;
+	
+protected:
+	
+	SoftEdgemap(const Blueprint& bp);
+	~SoftEdgemap();
+	
+	void 	_importSamples( SampleOrigo origo, const spx * pSource, int edgeBegin, int edgeEnd,
+						   int sampleBegin, int sampleEnd, int edgePitch, int samplePitch );
+	
+	void 	_importSamples( SampleOrigo origo, const float * pSource, int edgeBegin, int edgeEnd,
+						   int sampleBegin, int sampleEnd, int edgePitch, int samplePitch );
+	
+	int		_colorSegmentPitch() const { return m_size.w + m_size.h; }
+	
+	
+	char *	m_pBuffer;
+	spx *	m_pSamples;					// Stored vertically, e.g. samples for first column for all edges before samples for second column etc
+	
+	HiColor * m_pColors;				// Stored as horizontal + vertical tintmap colors for segment 1, then segment 2 etc.
+	Gradient * m_pGradients;
+	
+	std::bitset<maxSegments>	m_opaqueSegments;
+	std::bitset<maxSegments>	m_transparentSegments;
 
-	public:
-
-		//.____ Creation __________________________________________
-		
-		static SoftEdgemap_p	create( const Blueprint& blueprint );
-		static SoftEdgemap_p	create( const Blueprint& blueprint, SampleOrigo origo, const float * pSamples, int edges, int edgePitch = 0, int samplePitch = 0);
-		static SoftEdgemap_p	create( const Blueprint& blueprint, SampleOrigo origo, const spx * pSamples, int edges, int edgePitch = 0, int samplePitch = 0);
-
-
-    	//.____ Identification __________________________________________
-
-		const TypeInfo&     typeInfo(void) const override;
-		const static TypeInfo   TYPEINFO;
-
-		//.____ Appearance ____________________________________________________
-
-		bool	setColors( int begin, int end, const HiColor * pColors ) override;
-		bool	setGradients( int begin, int end, const Gradient * pGradients ) override;
-		
-		//.____ Content _______________________________________________________
-
-		bool 	importSamples( SampleOrigo origo, const spx * pSource, int edgeBegin, int edgeEnd,
-									  int sampleBegin, int sampleEnd, int edgePitch = 0, int samplePitch = 0 ) override;
-
-		bool 	importSamples( SampleOrigo origo, const float * pSource, int edgeBegin, int edgeEnd,
-									  int sampleBegin, int sampleEnd, int edgePitch = 0, int samplePitch = 0 ) override;
-
-		bool 	exportSamples( SampleOrigo origo, spx * pDestination, int edgeBegin, int edgeEnd,
-									  int sampleBegin, int sampleEnd, int edgePitch = 0, int samplePitch = 0 ) override;
-
-		bool 	exportSamples( SampleOrigo origo, float * pDestination, int edgeBegin, int edgeEnd,
-									  int sampleBegin, int sampleEnd, int edgePitch = 0, int samplePitch = 0 ) override;
-
-	protected:
-
-		SoftEdgemap(const Blueprint& bp);
-		~SoftEdgemap();
-
-		void 	_importSamples( SampleOrigo origo, const spx * pSource, int edgeBegin, int edgeEnd,
-									  int sampleBegin, int sampleEnd, int edgePitch, int samplePitch );
-
-		void 	_importSamples( SampleOrigo origo, const float * pSource, int edgeBegin, int edgeEnd,
-									  int sampleBegin, int sampleEnd, int edgePitch, int samplePitch );
-
-		void	_updateRenderColors();
-				
-		char *	m_pBuffer;
-		spx *	m_pSamples;					// Stored vertically, e.g. samples for first column for all edges before samples for second column etc
-
-		HiColor *	m_pRenderColors;		// Palette to be used in render call, converted as needed.
-		TintMode	m_tintMode;
-		
-   };
+};
 
 
 
