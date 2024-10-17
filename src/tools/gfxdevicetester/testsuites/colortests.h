df20ed144ecf0b9aaa1de2045aa857e760541ace
Tord Jansson <tord@nordicuitech.com>
2024-10-17 09:57:41 +0200

    GfxDeviceTester: Added missing file.

---
+++ src/tools/gfxdevicetester/testsuites/colortests.h
@@ -0,0 +1,32 @@
+#include "testsuite.h"
+
+class ColorTests : public TestSuite
+{
+public:
+	ColorTests()
+	{
+		name = "ColorTests";
+
+		addTest("ColorPalette", &ColorTests::drawPalette );
+	}
+
+
+
+
+	bool drawPalette(GfxDevice * pDevice, const RectSPX& canvas)
+	{
+		for (int i = 0; i < 7; i++)
+		{
+			RectSPX c = { i * 64, 0, 64, canvas.h };
+			pDevice->fill(c*64, m_rainbowColors[i] );
+		}
+
+		return true;
+	}
+
+
+private:
+
+	HiColor	m_rainbowColors[7] = { Color::Red, Color::Orange, Color::Yellow, Color::Green, Color::Blue, Color::Indigo, Color::Violet };
+
+};
