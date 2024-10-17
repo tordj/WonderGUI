9fa3e6399b453cca1c6fc135491a9757436ad341
Tord Jansson <tord@nordicuitech.com>
2024-10-17 17:00:41 +0200

    WIP: Edgemap refactoring to replace Tintmaps with colorstrips. Does not compile.

--- src/wondergfx/wg_gradyent.cpp
+++ src/wondergfx/wg_gradyent.cpp
@@ -42,6 +42,47 @@ namespace wg
 		return Gradyent_p(new Gradyent(top, bottom, left, right));
 	}
 
+
+	Gradyent_p Gradyent::create( const Gradient& gradient )
+	{
+		HiColor bottom( (gradient.bottomLeft.r + gradient.bottomRight.r) / 2,
+							(gradient.bottomLeft.g + gradient.bottomRight.g) / 2,
+							(gradient.bottomLeft.b + gradient.bottomRight.b) / 2,
+							(gradient.bottomLeft.a + gradient.bottomRight.a) / 2 );
+
+		HiColor top( (gradient.topLeft.r + gradient.topRight.r) / 2,
+			(gradient.topLeft.g + gradient.topRight.g) / 2,
+			(gradient.topLeft.b + gradient.topRight.b) / 2,
+			(gradient.topLeft.a + gradient.topRight.a) / 2);
+
+		HiColor left( (gradient.topLeft.r + gradient.bottomLeft.r) / 2,
+			(gradient.topLeft.g + gradient.bottomLeft.g) / 2,
+			(gradient.topLeft.b + gradient.bottomLeft.b) / 2,
+			(gradient.topLeft.a + gradient.bottomLeft.a) / 2);
+
+		HiColor right( (gradient.topRight.r + gradient.bottomRight.r) / 2,
+			(gradient.topRight.g + gradient.bottomRight.g) / 2,
+			(gradient.topRight.b + gradient.bottomRight.b) / 2,
+			(gradient.topRight.a + gradient.bottomRight.a) / 2);
+
+		float yDiff = (bottom.r - top.r) * (bottom.r - top.r) +
+						(bottom.g - top.g) * (bottom.g - top.g) +
+						(bottom.b - top.b) * (bottom.b - top.b) +
+						(bottom.a - top.a) * (bottom.a - top.a);
+
+		float xDiff = (left.r - right.r) * (left.r - right.r) +
+			(left.g - right.g) * (left.g - right.g) +
+			(left.b - right.b) * (left.b - right.b) +
+			(left.a - right.a) * (left.a - right.a);
+
+
+		if (xDiff > yDiff)
+			return Gradyent_p( new Gradyent(HiColor::White, HiColor::White, left, right) );
+		else
+			return Gradyent_p( new Gradyent(top, bottom, HiColor::White, HiColor::White) );
+	}
+
+
 	//____ constructor ________________________________________________________
 
 	Gradyent::Gradyent(HiColor top, HiColor bottom, HiColor left, HiColor right)
