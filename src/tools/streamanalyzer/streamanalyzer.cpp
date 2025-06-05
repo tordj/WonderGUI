f0f7f79e16f9d66b309bbc4c2b668e8c27987408
Tord Jansson <tord@nordicuitech.com>
2025-06-05 13:50:50 +0200

    WIP: State refactoring. BlockSkin working. ToggleButton etc now using Checked state.

--- src/tools/streamanalyzer/streamanalyzer.cpp
+++ src/tools/streamanalyzer/streamanalyzer.cpp
@@ -610,7 +610,7 @@ Widget_p MyApp::createNavigationPanel()
 		Base::msgRouter()->addRoute(pButton, MsgType::Select, [this,skip](Msg* pMsg)
 			{
 				m_recordedSteps.clear();
-				this->m_bRecordSteps = this->m_pRecordStepsToggle->isSelected();
+				this->m_bRecordSteps = this->m_pRecordStepsToggle->isChecked();
 				this->skipFrames( skip );
 				this->m_bRecordSteps = false;
 			});
@@ -679,7 +679,7 @@ Widget_p MyApp::createNavigationPanel()
 	{
 
 		m_recordedSteps.clear();
-		this->m_bRecordSteps = this->m_pRecordStepsToggle->isSelected();
+		this->m_bRecordSteps = this->m_pRecordStepsToggle->isChecked();
 
 		int frame = 235;
 		
@@ -790,7 +790,7 @@ bool MyApp::_loadSkins(Visitor * pVisitor)
 		_.axis = Axis::X,
 		_.frame = 4,
 		_.padding = 4,
-		_.states = { State::Default, State::Hovered, State::Selected, State::Selected + State::Hovered, State::Disabled }
+		_.states = { State::Default, State::Hovered, State::Checked, State::Checked + State::Hovered, State::Disabled }
 	));
 
 	m_pCheckBoxSkin = BlockSkin::create(WGBP(BlockSkin,
@@ -798,7 +798,7 @@ bool MyApp::_loadSkins(Visitor * pVisitor)
 		_.axis = Axis::Y,
 		_.frame = 3,
 //		_.defaultSize = { 12,12 },
-		_.states = { State::Default, State::Selected }
+		_.states = { State::Default, State::Checked }
 	));
 
 	m_pSectionSkin = BoxSkin::create(WGBP(BoxSkin,
